60b01346239fe4077ff7909c822d367be43846e9
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-06-21 16:07:34 -0300

    adds FileController.c

---
+++ t2fs/src/FilesController.c
@@ -0,0 +1,19 @@
+#include "../FilesController.h"
+#include "../t2fs.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int isValidName(char *name){
+    char atual;
+    int i = 0;
+    if(strlen(name) > 51 || strlen(name) < 0) return 1;
+    for(i = 0; i<strlen(name); i++) {
+        atual = name[i];
+        if(atual < '!' || atual > 'z') return 1;
+        //Verificar com Cechin se linha abaixo é necessária
+        //if(atual == '/') return 1;
+    }
+    return 0;
+}
