9dd357180ada1a8455146017b6c2fc4030e5c0a7
Scarlat7 <nataliarampon@hotmail.com>
2017-06-20 23:01:09 -0300

    Adds BootController skeleton

---
+++ t2fs/include/BootController.h
@@ -0,0 +1,15 @@
+/******************************
+* BOOT CONTROLLER COMPONENT
+* MAIN AUTHOR: NATÁLIA GUBIANI RAMPON - 00262502
+* 
+* Component responsible for booting the filesystem and this library
+*
+*******************************/
+
+#include <stdio.h>
+
+/* Initializes library */
+int init_lib();
+
+/* Reads boot section into memory */
+int read_boot_section();
