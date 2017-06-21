0b22f12e0d578a205cdcf4ec0ba298b1567e4028
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-06-21 16:03:15 -0300

    adds FilesController.h

---
+++ t2fs/include/FilesController.h
@@ -0,0 +1,13 @@
+/******************************
+* FILES CONTROLLER COMPONENT
+* MAIN AUTHORS: LAUREN SILVA ROLAN SAMPAIO - 00262517
+*               LEONARDO DA LUZ DORNELES - 00262516
+* 
+* Component responsible for manipulating files and directories
+*
+*******************************/
+
+#include <stdio.h>
+
+/* Verifies if de name passed as parameter is valid */
+int isValidName(char *name);
