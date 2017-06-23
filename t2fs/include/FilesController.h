4503edad98870901d39976cb532be96b6d9dae14
LaurenRolan <lauren_rolan@yahoo.com.br>
2017-06-23 15:22:36 -0300

    Adding printDirectory

--- t2fs/include/FilesController.h
+++ t2fs/include/FilesController.h
@@ -2,12 +2,15 @@
 * FILES CONTROLLER COMPONENT
 * MAIN AUTHORS: LAUREN SILVA ROLAN SAMPAIO - 00262517
 *               LEONARDO DA LUZ DORNELES - 00262516
-* 
+*
 * Component responsible for manipulating files and directories
 *
 *******************************/
 
 #include <stdio.h>
 
-/* Verifies if de name passed as parameter is valid */
+/* Verifies if the name passed as parameter is valid */
 int isValidName(char *name);
+
+/*Prints the contents of sector */
+void printDirectory(unsigned int sector);
--- t2fs/src/FilesController.c
+++ t2fs/src/FilesController.c
@@ -1,17 +1,61 @@
 #include "FilesController.h"
 #include "t2fs.h"
+#include "apidisk.h"
 
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 int isValidName(char *name){
-    char atual;
+    char current;
     int i = 0;
-    if(strlen(name) > 51 || strlen(name) < 0) return 1;
+    if(strlen(name) > 51 || strlen(name) < 0) return -1;
     for(i = 0; i<strlen(name); i++) {
-        atual = name[i];
-        if((atual < 'A' || atual > 'z') && (atual <'0' || atual > '9') && atual != '.') return 1;
+        current = name[i];
+        if((current < 'A' || current > 'z') && (current <'0' || current > '9') && current != '.') return -1;
     }
+    return 0;
+}
+
+//Função que imprime o conteúdo de um diretório, dado seu setor e tamanho em bytes
+//Ainda não testada
+int printDirectory(unsigned int sector, DWORD bytesFileSize){
+    unsigned char buffer[SECTOR_SIZE];
+	int index = 0;
+	int numberOfRegisters, numberOfSectors;
+    t2fs_record current;
+
+	if(ctrl == NULL){
+        init_lib();
+        return -1;
+	}
+
+    numberOfRegisters = bytesFileSize/64; //Diretórios são sempre múltiplos de 64, logo arredondamento não é necessário.
+    numberOfSectors = numberOfRegisters/4;
+    if(numberOfRegisters%4 != 0) numberOfSectors++;
+
+    do {
+        if(read_sector(0, buffer) != 0){
+            fprintf(stderr, "Falha ao ler setor do disco.\n");
+            return -1;
+        }
+
+        do {
+            memcpy(&current.TypeVal, buffer+index, sizeof(BYTE));
+            index += sizeof(BYTE);
+            memcpy(&current.name, buffer+index, MAX_FILE_NAME_SIZE*sizeof(CHAR));
+            index += MAX_FILE_NAME_SIZE*sizeof(CHAR);
+            memcpy(&current.blocksFileSize, buffer+index, sizeof(DWORD));
+            index += sizeof(DWORD);
+            memcpy(&current.bytesFileSize, buffer+index, sizeof(DWORD));
+            index += sizeof(DWORD);
+            memcpy(&current.MFTNumber, buffer+index, sizeof(DWORD));
+            index += sizeof(DWORD);
+            printf("Type: %02x\tName: %s\n", current.TypeVal, current.name);
+            printf("BlocksFileSize: %4d\tBytesFileSize: %4d\tMFTNumber: %10d\n", current.blocksFileSize, current.bytesFileSize, current.MFTNumber);
+            numberOfRegisters --;
+        }while(numberOfRegisters != 0 && (numberOfRegisters+1)%4 != 0);
+        numberOfSectors --;
+    }while(numberOfSectors != 0);
     return 0;
 }
