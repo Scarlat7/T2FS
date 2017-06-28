524c7e0e01543b77b5046f50d56e5939044d9b5d
Scarlat7 <nataliarampon@hotmail.com>
2017-06-28 16:50:30 -0300

    mapVBN skeleton

--- t2fs/src/TuplesController.c
+++ t2fs/src/TuplesController.c
@@ -7,6 +7,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define FIM_ENCADEAMENTO 0
+#define MAPEAMENTO 1
+#define MFT_ADICIONAL 2
+
 t_control ctrl;
 
 int searchMFT(int numReg, struct t2fs_4tupla *vector) {
@@ -38,10 +42,58 @@ int searchMFT(int numReg, struct t2fs_4tupla *vector) {
 	return 0;
 }
 
-int read_block_file(DWORD currentPointer, int nBlocks, BYTE* buffer) {
+int readSectorFile(FILE2 fileHandle, int nSectors, BYTE* buffer) {
+	
+	DWORD sector;
+	DWORD startVirtualBlock, block;
+	int i, j, k;
+	BYTE found = 0, bufferS[SECTOR_SIZE];
+	t2fs_4tupla bufferT[SECTOR_SIZE/sizeof(t2fs_4tupla)];
+
+	int currentMFT = ctrl.OpenFilesArray[fileHandle].MFT;
+	/*
+	j = 0;
+	do{
+		if(read_sector(RegisterToSector(currentMFT)+j, buffer) != 0){
+			fprintf(stderr, "Erro ao ler registro MFT do arquivo.\n");
+			return -1;
+		}
+	
+		startVirtualBlock = ctrl.OpenFilesArray[fileHandle].currentPointer/ctrl.blockSize;	
+		
+		i = 0;
+		
+		while(i < SECTOR_SIZE/sizeof(t2fs_4tupla) && found != 1){
+			if(bufferT[i].atributeType == MAPEAMENTO){
+				if((bufferT[i].virtualBlockNumber + bufferT[i].numberOfContiguousBlocks-1) <=  startVirtual && bufferT[i].virtualBlockNumber >= startVirtual){
+					found = 1;
+					for(k = 0; k < nSectors; k++){
+						block = (startVirtualBlock - bufferT[i].virtualBlockNumber)+bufferT[i].logicalBlockNumber;
+						sector = block*ctrl.blockSize;
+						sector += (ctrl.OpenFilesArray[fileHandle].currentPointer%blockSize)/SECTOR_SIZE;
+						read_sector(sector, bufferS);
+						memcpy(buffer+index, bufferS, SECTOR_SIZE);
+						index += SECTOR_SIZE;
+					}
+
+				}
+				i++;
+			}
+		}
+	
+		if(bufferT[i-1].attributeType == MFT_ADICIONAL){
+			currentMFT = bufferT[i-1].virtualBlockNumber;
+			j = 0;
+		}else{
+			j+=1;
+		}
+		 
+	}while(found != 1);
+	*/
+
 	return -1;
 }
 
-int map_VBN(DWORD MFT, DWORD VBN, DWORD* LBN) {
+int mapVBN(DWORD MFT, DWORD VBN, DWORD* LBN) {
 	return -1;
 }
