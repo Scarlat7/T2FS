06b7b953af701009f119ed2d75005deb45ec212a
leodorna <leonardo.dorneles@inf.ufrgs.br>
2017-07-09 22:25:29 -0300

    tirei os códigos comentados

--- t2fs/src/t2fs.c
+++ t2fs/src/t2fs.c
@@ -113,29 +113,8 @@ int read2 (FILE2 handle, char *buffer, int size){
 
 	if(readRequestedSectors(handle, actualReadSize, leitura) == ERROR) return ERROR;
 
-	/*for(i = 0; i < n_blocks-end_pointer; i++){
-		mapVBN(mft, i+end_pointer, &currentLB);
-		mapLBN(currentLB, &initial_sector);
-		for(j = 0; j < (ctrl.boot.blockSize - offset); j++){
-			read_sector(initial_sector+ offset+j, escrita+i*BLOCK_SIZE+j*SECTOR_SIZE);
-			remaining--;
-			if(remaining == 0) break;
-		}
-		if(remaining == 0) break;
-	}*/
-
 	memcpy(buffer, leitura+relativeByte, size);
 	ctrl.openFilesArray[handle].currentPointer += actualReadSize;
-
-	/*BYTE *bufferAux;
-	DWORD sectors = bytesToSectors(size);
-	DWORD relativeByte = openFilesArray[handle].currentPointer % SIZE_SECTOR;	
-	
-	bufferAux = malloc(sectors*sizeof(BYTE));
-
-	if(readSectorFile(handle, sectors, bufferAux))
-		memcpy(buffer, bufferAux+relativeByte, size);
-	else return ERROR;	*/
 
 	return actualReadSize;
 }
@@ -146,13 +125,6 @@ int write2(FILE2 handle, char *buffer, int size){
 	if(isOpen(ctrl.openFilesArray[handle].name, TYPEVAL_REGULAR) <= 0) 
 		return ERROR;
 
-	/*int BLOCK_SIZE = ctrl.boot.blockSize*SECTOR_SIZE;
-	DWORD mft = ctrl.openFilesArray[handle].MFT, currentLB, initial_sector;
-	DWORD n_blocks = ctrl.openFilesArray[handle].bytesSize/(ctrl.boot.blockSize*SECTOR_SIZE)+1;
-	DWORD end_pointer = ctrl.openFilesArray[handle].currentPointer/(ctrl.boot.blockSize*SECTOR_SIZE);
-	DWORD offset = (ctrl.openFilesArray[handle].currentPointer%(ctrl.boot.blockSize*SECTOR_SIZE))/SECTOR_SIZE;
-	int remaining = n_sectors;
-	*/
 	DWORD relativeByte = ctrl.openFilesArray[handle].currentPointer % SECTOR_SIZE;
 
 	DWORD n_sectors = size/SECTOR_SIZE;
@@ -163,32 +135,9 @@ int write2(FILE2 handle, char *buffer, int size){
 
 	if(readRequestedSectors(handle, size, escrita) == ERROR) return ERROR;
 
-	/*for(i = 0; i < n_blocks-end_pointer; i++){
-		mapVBN(mft, i+end_pointer, &currentLB);
-		mapLBN(currentLB, &initial_sector);
-		for(j = 0; j < (ctrl.boot.blockSize - offset); j++){
-			read_sector(initial_sector+ offset+j, escrita+i*BLOCK_SIZE+j*SECTOR_SIZE);
-			remaining--;
-			if(remaining == 0) break;
-		}
-		if(remaining == 0) break;
-	}*/
-
 	memcpy(escrita+relativeByte, buffer, size);
 
 	if(writeRequestedSectors(handle, size, escrita) == ERROR) return ERROR;
-
-	/*remaining = n_sectors;
-	for(i = 0; i < n_blocks-end_pointer; i++){
-		mapVBN(mft, i+end_pointer, &currentLB);
-		mapLBN(currentLB, &initial_sector);
-		for(j = 0; j < (ctrl.boot.blockSize - offset); j++){
-			write_sector(initial_sector+offset+j, escrita+i*BLOCK_SIZE+j*SECTOR_SIZE);
-			remaining--;
-			if(remaining == 0) break;
-		}
-		if(remaining == 0) break;
-	}*/
 
 	ctrl.openFilesArray[handle].bytesSize += updateFileSize(ctrl.openFilesArray[handle], size);
 	ctrl.openFilesArray[handle].currentPointer += size;
